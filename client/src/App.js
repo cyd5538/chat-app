2217139dd8b9410d32cb2ecdd616bdd7b516943a
cyd5538 <cyd5538@naver.com>
2022-09-22 22:01:50 +0900

    회원가입 페이지 ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Chat from './pages/Chat'
 const App = () => {
   return (
     <Routes>
