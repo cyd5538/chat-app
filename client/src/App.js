2481552a32d221bcc9e08ca4c2e21dc3ff0f4ccd
cyd5538 <cyd5538@naver.com>
2022-09-25 21:40:11 +0900

    아바타 설정  페이지 ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import {Routes, Route} from 'react-router-dom';
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Chat from './pages/Chat'
+import SetAvatar from './pages/SetAvatar';
 const App = () => {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Chat />} />
+      <Route path="/setAvatar" element={<SetAvatar />} />
     </Routes>
   )
 }
